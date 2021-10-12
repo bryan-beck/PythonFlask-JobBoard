3cec6d15aae460a55d1707aef363a384b1499433
Bryan Beck <88339907+bryan-beck@users.noreply.github.com>
2021-10-12 06:49:37 -0700

    'Completed'

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
+path = ('db/jobs.sqlite')
 app = Flask(__name__)
-
+def open_connection():
+    getattr('_connection')
 @app.route('/')
 @app.route('/jobs')
 def jobs():
